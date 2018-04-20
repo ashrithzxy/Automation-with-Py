a9018c1808faa559ab289f6ec6180b93785bed3f
Ashrith <35345514+ashrithzxy@users.noreply.github.com>
2018-04-20 23:39:26 +0530

    Add files via upload

--- FileCompare.py
+++ FileCompare.py
@@ -5,4 +5,4 @@ fdir1 = os.listdir("folder1")
 fdir2 = os.listdir("folder2")
 diff = list(set(fdir2) - set(fdir1))
 for file in diff:
-    shutil.move(os.path.join(source, file), "Destination Path")
+    shutil.copy(os.path.join(source, file), "Destination Path")
